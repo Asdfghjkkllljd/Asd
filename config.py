08d1be31d6c15626ddb340b51daf625fb3f34320
Asdfghjkkllljd <staricinal49@gmail.com>
2024-09-01 20:03:46 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+TOKEN = "7525375093:AAEZG0M1kAT6RsgYvOKhAhj30CO4y_Q5gVY" # Токен бота
+CHANNEL_ID = -1002056388271 # ID канала со ставками
+ADMINS = [6372950353] # ID админов
+BET_URL = "http://t.me/send?start=IV7At7pCRjY0" # Ссылка на счет
+CRYPTOPAY_TOKEN = "260414:AAT42GCmdNAYFFWsb39Qj8n3qb61Hfes5oa" # Тут CryptoPay токен
+WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
+LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
+LOGS_ID = -1002241301442 # ID канала с оплатами
+
+RULES_LINK = "https://t.me/FLIPPY_PRAVILA" # Ссылка на канал с правилами
+NEWS_LINK = "https://t.me/flippyNOVOSTI" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/official_neender" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "https://t.me/Flippy_Casino" # Ссылка на переходник
+BETS_LINK = "https://t.me/Flippy_Casino" # Ссылка на канал ставок
+CASINO_NAME = "FLIPPY CASINO" # Название казино
